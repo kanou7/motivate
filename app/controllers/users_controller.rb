3a1749fd63dcf113f051b63a4181bd0f49d859f3
kanou7 <kanounba7@icloud.com>
2021-05-16 15:48:05 +0900

    マイページ基本情報表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -9,4 +10,9 @@ class UsersController < ApplicationController
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
+
+  def show
+    @user = User.find((params[:id]))
+  end
+
 end
