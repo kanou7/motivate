c02f9df1a9ebe829c49daedb9a8117817a11bdf2
kanou7 <kanounba7@icloud.com>
2021-05-21 16:02:15 +0900

    tweet投稿機能結合テストの調整

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -14,11 +14,12 @@ RSpec.describe 'ツイート投稿', type: :system do
       find('input[name="commit"]').click
       expect(current_path).to eq(root_path)
       # 新規投稿ページへのボタンがあることを確認する
-      expect(page).to have_content('投稿')
+      expect(page).to have_css('a', class: "tweet-btn")
       # 投稿ページに移動する
       visit new_tweet_path
       # フォームに情報を入力する
       fill_in '作品名', with: @tweet.title
+      attach_file('画像', 'public/images/sample.png', make_visible: true)
       fill_in '説明文', with: @tweet.text
       select '学生', from: 'tweet[job_id]'
       select '中退', from: 'tweet[status_id]'
@@ -28,14 +29,20 @@ RSpec.describe 'ツイート投稿', type: :system do
       end.to change { Tweet.count }.by(1)
       # トップページに遷移することを確認する
       expect(current_path).to eq(root_path)
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（タイトル）
+      expect(page).to have_content(@tweet.title)
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
-      # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_selector("img[src$='sample.png']")
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（説明文）
+      expect(page).to have_content(@tweet.text)
     end
   end
   context 'ツイート投稿ができないとき' do
     it 'ログインしていないと新規投稿ページに遷移できない' do
       # トップページに遷移する
+      visit root_path
       # 新規投稿ページへのボタンがないことを確認する
+      expect(page).to have_no_css('a', class: "tweet-btn")
     end
   end
 end
