7f21d74e040330b1eea0b3b99b19c051612c3daa
Kouki Kanou <kanounba7@icloud.com>
2021-07-02 17:00:32 +0900

    rspecテスト修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -11,6 +11,16 @@ class Tweet < ApplicationRecord
   belongs_to :job
   belongs_to :status
 
+  with_options presence: true do
+    validates :title
+    validates :text
+
+    with_options numericality: { other_than: 1, message: 'を選択してください' } do
+      validates :job_id
+      validates :status_id
+    end
+  end
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
