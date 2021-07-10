e2b28fd6f7be63dff13e499ef3c76deaafd9a703
Kouki Kanou <kanounba7@icloud.com>
2021-07-10 16:10:57 +0900

    ER図修正

--- db/migrate/20210512115125_create_tweets.rb
+++ db/migrate/20210512115125_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
       t.string :title,     null: false
+      t.text :message
       t.text :text,        null: false
       t.integer :job_id,   null: false
       t.integer :status_id,null: false
