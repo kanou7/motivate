3153c0d0e802e283c16707d555a94418042112c0
Kouki Kanou <kanounba7@icloud.com>
2021-07-10 18:15:38 +0900

    messageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_032723) do
+ActiveRecord::Schema.define(version: 2021_07_10_075556) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_032723) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "message"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
