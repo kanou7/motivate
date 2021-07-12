a63d42076d76c8e4413df750aa248e34e8cf7a0b
Kouki Kanou <kanounba7@icloud.com>
2021-07-12 12:12:39 +0900

    fadeIn

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -16,6 +16,23 @@ $( function() {
       }, 500);
       return false;
     });
-    
+
   })();
+
+  $(window).on('load scroll',function (){
+		$('.fadeIn').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('fade-active');
+			}
+		});
+	});
+
 });
